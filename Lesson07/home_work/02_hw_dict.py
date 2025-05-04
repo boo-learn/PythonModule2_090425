d8570ef46636864b8bbefaee2c2946ce5e26eea4
ormosh-habriella <ormoshg@gmail.com>
2025-05-04 19:55:20 +0200

    Update 02_hw_dict.py

--- Lesson07/home_work/02_hw_dict.py
+++ Lesson07/home_work/02_hw_dict.py
@@ -27,21 +27,47 @@ staff = [
     },
 ]
 # Вычислите:
-print("Имя и Фамилию сотрудника с самой высокой зарплатой:")
+max_salary = 0
+max_salary_employee = {}
 
-# TODO: your code here
+for person in staff:
+    if person["salary"] > max_salary:
+        max_salary = person["salary"]
+        max_salary_employee = person
 
-print("Имя и Фамилию сотрудника с самой низкой зарплатой:")
+print("Имя и Фамилия сотрудника с самой высокой зарплатой:", max_salary_employee["name"], max_salary_employee["surname"] )
 
-# TODO: your code here
+min_salary = staff[0]["salary"]
+min_salary_employee = staff[0]
 
-print("Среднеарифметическую зарплату всех сотрудников")
+for person in staff:
+    if person["salary"] < min_salary:
+        min_salary = person["salary"]
+        min_salary_employee = person
 
-# TODO: your code here
+print("Имя и Фамилия сотрудника с самой низкой зарплатой:", min_salary_employee["name"], min_salary_employee["surname"])
 
-print("Количество однофамильцев в организации")
+total_salary = 0
 
-# TODO: your code here
+for person in staff:
+    total_salary += person["salary"]
+
+average_salary = total_salary / len(staff)
+
+print("Среднеарифметическуя зарплату всех сотрудников:", average_salary)
+
+same_surname_count = {}
+
+for person in staff:
+    surname = person["surname"]
+    if surname in same_surname_count:
+        same_surname_count[surname] += 1
+    else:
+        same_surname_count[surname] = 1
+
+for surname, count in same_surname_count.items():
+    if count > 1:
+        print(f"Фамилия '{surname}' встречается {count} раза ")
 
 print("*Список всех сотрудников(Имя и Фамилию) в порядке возрастания их зарплаты")
 
