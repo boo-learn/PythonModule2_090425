a34e4e17c0c848a0aa07acb3ffb44f63aa61feef
ormosh-habriella <ormoshg@gmail.com>
2025-05-18 22:00:11 +0200

    Update 02_hw_transform.py

--- Lesson17/home_work/02_hw_transform.py
+++ Lesson17/home_work/02_hw_transform.py
@@ -2,3 +2,7 @@
 # Вам нужно получить список длин всех строк, которые имеют длину более 3 символов.
 
 words = ["cat", "dog", "elephant", "mouse", "bird", "ant"]
+
+string_lengths = list(map(len, filter(lambda word: len(word) > 3, words)))
+
+print(string_lengths)
