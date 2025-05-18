1068b00affdde3dc5954ee150b72200b6d9aff45
ormosh-habriella <ormoshg@gmail.com>
2025-05-18 21:59:04 +0200

    Update 01_hw_transform.py

--- Lesson17/home_work/01_hw_transform.py
+++ Lesson17/home_work/01_hw_transform.py
@@ -2,4 +2,7 @@
 # Вам нужно применить скидку в 10% к каждому товару, цена которого превышает 100 единиц, и вывести новые цены.
 
 prices = [50, 120, 80, 150, 90, 200]
-...
+
+discounted_prices = list(map(lambda price: price * 0.9 if price > 100 else price, prices))
+
+print(discounted_prices)
