fe3770ba5f1d439dd88c85f939632ef69ebb7f00
ormosh-habriella <ormoshg@gmail.com>
2025-05-18 23:26:52 +0200

    Update 01_hw_generator.py

--- Lesson16/home_work/01_hw_generator.py
+++ Lesson16/home_work/01_hw_generator.py
@@ -1,3 +1,14 @@
 # "Генерация простых множителей числа"
 # Напишите генераторную функцию, которая принимает целое число n и генерирует его простые множители по одному.
 # (Например, для 12 генератор должен выдавать 2, 2, 3
+def gen_prime_factors(n):
+    divisor = 2
+    while n > 1:
+        if n % divisor == 0:
+            yield divisor
+            n //= divisor
+        else:
+            divisor += 1
+
+for factor in gen_prime_factors(12):
+    print(factor)
